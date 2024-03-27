7d48f74035b9a84d7a6fed9b3de64e3a7d72303c
shubhamsingh83 <akashsingh832001@gmail.com>
2024-03-27 16:36:31 +0530

    page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const[userName, setUsername] = useState("");
+  const[password,setPassword] = useState("");
+  const[error,setError] = useState("");
+  const[isSubmitted,setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e)=>{
+     e.preventDefault();
+     if(userName === "user" && password === "password"){
+      setError("");
+      setIsSubmitted(true);
+     }
+     else{
+      setError("Invalid username or password");
+      setIsSubmitted(false);
+     }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <h1>Login Page</h1>
+       {isSubmitted ? (
+           <div>
+            <p>Welcome, {userName}</p>
+           </div>
+        ):(
+             <form onSubmit={handleSubmit}>
+              {error && <p>{error}</p>}
+              <div>
+                <label htmlFor="username">Username:</label>
+                <input
+                   onChange={(e)=>setUsername(e.target.value)}
+                   type="text"
+                   value={userName}
+                   required
+                   placeholder="username"
+                   id="username"
+                />
+              <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                   onChange={(e) => setPassword(e.target.value)}
+                   type="password"
+                   value={password}
+                   required
+                   placeholder="password"
+                   id="password"
+               />
+              </div>
+              <div>
+                <button type="submit">Submit</button>
+              </div>
+              </div>
+             </form>
+        )}
     </div>
   );
 }
